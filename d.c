edb1e455c29b1c6dfb911a3c9e5f23dce20214c0
rohitxthapa <rohitxthapa@gmail.com>
2026-02-12 20:45:39 +0545

    i forgot to add before commint so changes for previous commit

--- d.c
+++ d.c
@@ -9,21 +9,18 @@ typedef struct {
   double delta;
   int animationspeed;
   SDL_Texture *map;
-  float x, y, w, h;
   SDL_FRect swindow;
   SDL_FRect dwindow;
+  Uint64 framestarttick, frameendtick;
 } Gamestate;
 
 typedef struct {
-  int drectheight, drectbreadth;
   SDL_FRect drect;
-  int srectheight, srectbreadth;
   SDL_FRect srect;
   SDL_Texture *texture;
   int dx, dy;
   int ax, ay;
   int framecount;
-  Uint64 framestarttick, frameendtick;
   float playerspeed;
 } Character;
 
@@ -36,79 +33,18 @@ typedef struct {
 
 } Object;
 
-void init(SDL_Window **window, SDL_Renderer **renderer, Gamestate *state);
-void loadmedia(SDL_Renderer *renderer, SDL_Texture **player, SDL_Texture **map);
-void handleinput(Character *player);
-void update(Character *player, Object *objs, int size, Gamestate *state);
-void render(SDL_Renderer *renderer, Character *player, Object *objs, int size,
-            Gamestate *state);
-void cleanup(const char *error, void *win, void *ren, void *tex);
-
-int main(int argv, char *argc[]) {
-
-  Gamestate state = {
-      .windowbreadth = 1000,
-      .windowheight = 800,
-      .delta = 0.016,
-      .animationspeed = 100,
-      .x = 300.0f,
-      .y = 300.0f,
-      .w = 400.0f,
-      .h = 400.0f,
-      .swindow = {state.x, state.y, state.w, state.h},
-      .dwindow = {0, 0, .w = state.windowbreadth, .h = state.windowheight}};
-  SDL_Window *window;
-  SDL_Renderer *renderer;
-  init(&window, &renderer, &state);
-
-  Character player = {
-      .drectbreadth = 100,
-      .drectheight = 100,
-      .srectbreadth = 48,
-      .srectheight = 48,
-      .srect = {0, 0, player.srectbreadth, player.srectheight},
-      .drect = {(state.windowbreadth >> 1) - (player.drectbreadth >> 1),
-                (state.windowheight >> 1) - (player.drectheight >> 1),
-                player.drectbreadth, player.drectheight},
-      .dx = 0,
-      .dy = 0,
-      .ax = 0,
-      .ay = 0,
-      .framecount = 0,
-      .playerspeed = 120.0f,
-      .framestarttick = SDL_GetTicks()};
-  loadmedia(renderer, &player.texture, &state.map);
-
-  Object objs[10] = {
-      {{400 - state.x, 400 - state.y, 100, 100}, 0xff, 0x00, 0x00, 0xff},
-      {{600 - state.x, 600 - state.y, 100, 100}, 0x00, 0xff, 0x00, 0xff},
-  };
-
-  int running = 1;
-  SDL_Event event;
-
-  Uint64 start = SDL_GetPerformanceCounter(), end;
-  while (running) {
-
-    end = SDL_GetPerformanceCounter();
-    state.delta = (double)(end - start) / (double)SDL_GetPerformanceFrequency();
-    start = end;
-
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_EVENT_QUIT) {
-        running = 0;
-      }
-    }
-
-    handleinput(&player);
-    update(&player, objs, 2, &state);
-    render(renderer, &player, objs, 2, &state);
+void cleanup(const char *error, void *win, void *ren, void *tex) {
+  printf("%s", error);
+  if (tex) {
+    SDL_DestroyTexture(tex);
   }
-
-  const char *message = "execution completed";
-  cleanup(message, window, renderer, &player.texture);
+  if (ren) {
+    SDL_DestroyRenderer(ren);
+  }
+  if (win) {
+    SDL_DestroyWindow(win);
+  }
   SDL_Quit();
-  return 0;
 }
 
 void init(SDL_Window **window, SDL_Renderer **renderer, Gamestate *state) {
@@ -148,7 +84,7 @@ void loadmedia(SDL_Renderer *renderer, SDL_Texture **player,
     exit(1);
   }
   *player = SDL_CreateTextureFromSurface(renderer, surface);
-  surface = SDL_LoadBMP("maptry.bmp");
+  surface = SDL_LoadBMP("greenscreen.bmp");
   *map = SDL_CreateTextureFromSurface(renderer, surface);
   SDL_DestroySurface(surface);
   if (*player == NULL || *map == NULL) {
@@ -186,7 +122,15 @@ void update(Character *player, Object *objs, int size, Gamestate *state) {
 
   if (moving) {
     player->drect.x += player->dx * player->playerspeed * state->delta;
+    state->swindow.x += player->dx * player->playerspeed * state->delta;
     player->drect.y += player->dy * player->playerspeed * state->delta;
+    state->swindow.y += player->dy * player->playerspeed * state->delta;
+
+    for (int i = 0; i < size; i++) {
+      objs[i].position.x = 400 - state->swindow.x;
+      objs[i].position.y = 600 - state->swindow.y;
+    }
+
     if (player->dx != 0) {
       player->dx += (player->dx < 0) ? 1 : -1;
     }
@@ -195,8 +139,9 @@ void update(Character *player, Object *objs, int size, Gamestate *state) {
     }
   }
 
-  player->frameendtick = SDL_GetTicks();
-  if (state->animationspeed < (player->frameendtick - player->framestarttick)) {
+  state->frameendtick = SDL_GetTicks();
+  if (((state->frameendtick - state->framestarttick) / state->animationspeed) !=
+      0) {
     if (moving) {
       player->framecount = (player->framecount + 1) % 4;
       player->srect.x = player->framecount * 96;
@@ -204,14 +149,14 @@ void update(Character *player, Object *objs, int size, Gamestate *state) {
       player->framecount = 0;
       player->srect.x = 0;
     }
-    player->framestarttick = player->frameendtick;
+    state->framestarttick = state->frameendtick;
   }
 
   if (objs) {
     for (int i = 0; i < size; i++) {
-      if (player->drect.y + player->drectheight >= objs[i].position.y) {
+      if (player->drect.y + player->drect.h >= objs[i].position.y) {
         if (player->drect.y <= objs[i].position.y + objs[i].position.h) {
-          if (player->drect.x + player->drectbreadth >= objs[i].position.x) {
+          if (player->drect.x + player->drect.w >= objs[i].position.x) {
             if (player->drect.x <= objs[i].position.x + objs[i].position.w) {
 
               player->dx -= player->ax * 18;
@@ -222,25 +167,22 @@ void update(Character *player, Object *objs, int size, Gamestate *state) {
       }
     }
   }
+
   if (player->drect.x < 0) {
     player->drect.x = 0;
     player->dx = 0;
-    // player->dx += 20;
   }
   if (player->drect.y < 0) {
     player->drect.y = 0;
     player->dy = 0;
-    // player->dy += 20;
   }
-  if ((player->drect.x + player->drectbreadth) > state->windowbreadth) {
-    player->drect.x = state->windowbreadth - player->drectbreadth;
+  if ((player->drect.x + player->drect.w) > state->windowbreadth) {
+    player->drect.x = state->windowbreadth - player->drect.w;
     player->dx = 0;
-    // player->dx -= 20;
   }
-  if ((player->drect.y + player->drectheight) > state->windowheight) {
-    player->drect.y = state->windowheight - player->drectheight;
+  if ((player->drect.y + player->drect.h) > state->windowheight) {
+    player->drect.y = state->windowheight - player->drect.h;
     player->dy = 0;
-    // player->dy -= 20;
   }
 }
 
@@ -258,16 +200,75 @@ void render(SDL_Renderer *renderer, Character *player, Object *objs, int size,
   SDL_RenderPresent(renderer);
 }
 
-void cleanup(const char *error, void *win, void *ren, void *tex) {
-  printf("%s", error);
-  if (tex) {
-    SDL_DestroyTexture(tex);
+// void init(SDL_Window **window, SDL_Renderer **renderer, Gamestate *state);
+// void loadmedia(SDL_Renderer *renderer, SDL_Texture **player, SDL_Texture
+// **map); void handleinput(Character *player); void update(Character *player,
+// Object *objs, int size, Gamestate *state); void render(SDL_Renderer
+// *renderer, Character *player, Object *objs, int size,
+//             Gamestate *state);
+// void cleanup(const char *error, void *win, void *ren, void *tex);
+
+int main(int argv, char *argc[]) {
+
+  Gamestate state = {
+      .windowbreadth = 1000,
+      .windowheight = 800,
+      .delta = 0.016,
+      .animationspeed = 100,
+      .swindow = {300, 300, 400, 400},
+      .framestarttick = SDL_GetTicks(),
+      .dwindow = {0, 0, .w = state.windowbreadth, .h = state.windowheight}};
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  init(&window, &renderer, &state);
+
+  Character player = {.srect = {0, 0, 48, 48},
+                      .drect = {(state.windowbreadth >> 1) - 50,
+                                (state.windowheight >> 1) - 50, 100, 100},
+                      .dx = 0,
+                      .dy = 0,
+                      .ax = 0,
+                      .ay = 0,
+                      .framecount = 0,
+                      .playerspeed = 120.0f};
+  loadmedia(renderer, &player.texture, &state.map);
+
+  Object objs[10] = {
+      {{400 - state.swindow.x, 400 - state.swindow.y, 100, 100},
+       0xff,
+       0x00,
+       0x00,
+       0xff},
+      {{600 - state.swindow.x, 800 - state.swindow.y, 100, 100},
+       0x00,
+       0xff,
+       0x00,
+       0xff},
+  };
+
+  int running = 1;
+  SDL_Event event;
+
+  Uint64 start = SDL_GetPerformanceCounter(), end;
+  while (running) {
+
+    end = SDL_GetPerformanceCounter();
+    state.delta = (double)(end - start) / (double)SDL_GetPerformanceFrequency();
+    start = end;
+
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = 0;
+      }
+    }
+
+    handleinput(&player);
+    update(&player, objs, 2, &state);
+    render(renderer, &player, objs, 2, &state);
   }
-  if (ren) {
-    SDL_DestroyRenderer(ren);
-  }
-  if (win) {
-    SDL_DestroyWindow(win);
-  }
+
+  const char *message = "execution completed";
+  cleanup(message, window, renderer, &player.texture);
   SDL_Quit();
+  return 0;
 }
