f4e0b27e9e228831ad1f1c7efd804ecc8ebf40d7
rohitxthapa <rohitxthapa@gmail.com>
2026-02-16 09:45:01 +0545

    i am going to make a procedually generated 2d space so i am trying to make solid shaped it runs not correctly need to debug

--- shapes.c
+++ shapes.c
@@ -2,46 +2,135 @@
 #include <SDL3/SDL_main.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-enum shapes {
-  triangle,
-  rectangle,
-  pentagon,
-  hexagon,
-  heptagon,
-  octagon,
-  nonagon,
-  decacgon
-};
+float worldcoordinatex = 0.0f, worldcoordinatey = 0.0f;
+float chunklengthx = 4000.0, chunklenthy = 4000.0;
 
-int main() {
-  int n = 4;
+typedef struct {
+  int sides;
+  SDL_Vertex *vertices;
+  float x, y;
+  float size;
+  float degree;
+  float degreeradian;
+  float degreetoxaxis;
+} Blocks;
 
-  double degree = (n - 2) * (180.0 / n);
-  printf("%f", degree);
-  double degreeradian = degree * (3.1415926 / 180);
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_Window *window;
-  window = SDL_CreateWindow("shapes", 1920, 1080, SDL_WINDOW_RESIZABLE);
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
-  SDL_SetRenderVSync(renderer, 1);
-  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xff, 0xff);
+bool init(SDL_Window **window, SDL_Renderer **renderer) {
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("couldnt initialize! : %s", SDL_GetError());
+    return false;
+  }
+  *window = SDL_CreateWindow("shapes", 1920, 1080, SDL_WINDOW_RESIZABLE);
+  if (*window == NULL) {
+    SDL_Log("error in creating window ! : %s", SDL_GetError());
+    return false;
+  }
+  *renderer = SDL_CreateRenderer(*window, NULL);
+  if (*renderer == NULL) {
+    SDL_Log("error in creating renderer ! : %s", SDL_GetError());
+    SDL_DestroyWindow(*window);
+    return false;
+  }
+  if (SDL_SetRenderVSync(*renderer, 1)) {
+    SDL_Log("VSync activated");
+  } else {
+    SDL_Log("VSync couldn't activate");
+  }
+  return true;
+}
 
-  double ix = 600, iy = 800, fx = 700, fy = 800;
-  double tx, ty;
-  SDL_RenderLine(renderer, ix, iy, fx, fy);
-  for (int i = 1; i < n; i++) {
+void getblocksdimentions(Blocks *block) {
 
-    tx = fx + (ix - fx) * cos(degreeradian) - (iy - fy) * sin(degreeradian);
-    ty = fy + (ix - fx) * sin(degreeradian) + (iy - fy) * cos(degreeradian);
-    SDL_RenderLine(renderer, fx, fy, tx, ty);
+  block->vertices = malloc(120);
+
+  block->x = 200;
+  block->y = 200;
+  block->sides = 3 + rand() % 10;
+  block->degree = (block->sides - 2) * 180.0 / block->sides;
+  block->degreeradian = block->degree * (3.141592653589) / 180;
+  block->size = 50 + rand() % 50;
+  block->degreetoxaxis = 360 * (sin(rand() % 90));
+
+  return;
+}
+
+void updateblocks(Blocks *block) {
+  double tempx, tempy;
+  double ix, iy, fx, fy;
+
+  ix = block->x;
+  fx = ix + (block->size * cos(block->degreetoxaxis));
+  iy = block->y;
+  fy = iy - (block->size * sin(block->degreetoxaxis));
+  for (int i = 0; i < block->sides; i++) {
+
+    tempx = fx + (ix - fx) * cos(block->degreeradian) -
+            (iy - fy) * sin(block->degreeradian);
+    fx = ix + block->size;
+    tempy = fy + (ix - fx) * sin(block->degreeradian) -
+            (iy - fy) * cos(block->degreeradian);
+    block->vertices[i].position.x = ix;
+    block->vertices[i].position.y = iy;
     ix = fx;
     iy = fy;
-    fx = tx;
-    fy = ty;
+    fx = tempx;
+    fy = tempy;
   }
+  block->vertices[block->sides].position.x = ix;
+  block->vertices[block->sides].position.y = iy;
+
+  for (int i = 0; i <= block->sides; i++)
+    block->vertices[i].color = (SDL_FColor){1, 0, 0, 1};
+  return;
+}
+
+void renderblocks(Blocks *block, SDL_Renderer *renderer) {
+  int triangles = block->sides - 1;
+  int incides[triangles * 3];
+  int n = 0;
+  for (int i = 0; i < triangles; i++) {
+
+    for (int j = 0; j < 3; j++) {
+      incides[i * 3 + j] = (j == 0) ? 0 : j + n;
+    }
+    n++;
+  }
+  SDL_RenderGeometry(renderer, NULL, block->vertices, block->sides + 1, incides,
+                     triangles * 3);
+}
+
+int main() {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  if (!init(&window, &renderer)) {
+    return 1;
+  }
+  Blocks block;
+
+  getblocksdimentions(&block);
+  updateblocks(&block);
+
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xff, 0xff);
+  renderblocks((&block), renderer);
   SDL_RenderPresent(renderer);
-  SDL_Delay(6000);
+
+  srand(12345);
+  SDL_Event event;
+  bool running = true;
+  while (running) {
+
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = false;
+      }
+    }
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xff, 0xff);
+
+    SDL_RenderPresent(renderer);
+  }
+
   SDL_DestroyWindow(window);
   SDL_DestroyRenderer(renderer);
   SDL_Quit();
